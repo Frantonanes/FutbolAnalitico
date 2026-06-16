1f9d2d05ee4966ba2c8256bcb20536fe67660a09
FranTonanes <francoetonanes@gmail.com>
2026-06-16 12:15:01 -0300

    editnewspage

--- frontend/src/features/admin/pages/DashboardPage.tsx
+++ frontend/src/features/admin/pages/DashboardPage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 
 import { getNews } from '../../../services/newsService'
 import { getPredictions } from '../../../services/predictionService'
+import './DashboardPage.css'
 
 export default function DashboardPage() {
   const [newsCount, setNewsCount] = useState(0)
