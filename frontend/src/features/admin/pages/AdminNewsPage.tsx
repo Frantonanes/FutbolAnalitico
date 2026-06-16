114970acf5200209c78e02640efc0b002763f880
FranTonanes <francoetonanes@gmail.com>
2026-06-16 17:06:26 -0300

    fix media update

--- frontend/src/features/admin/pages/AdminNewsPage.tsx
+++ frontend/src/features/admin/pages/AdminNewsPage.tsx
@@ -97,10 +97,10 @@ export default function AdminNewsPage() {
                   </td>
 
                   <td>
-  {item.createdAt
-    ? new Date(item.createdAt).toLocaleDateString('es-AR')
-    : '-'}
-</td>
+                    {item.createdAt
+                      ? new Date(item.createdAt).toLocaleDateString('es-AR')
+                      : '-'}
+                  </td>
 
                   <td>
                     <div className="admin-actions">
